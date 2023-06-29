7f56782877097b42baafb12c926fd4bbb48283bd
RubenF <rubenf@campus.technion.ac.il>
2023-06-29 19:23:33 +0300

    challenge 0-2 done

--- malloc_3.cpp
+++ malloc_3.cpp
@@ -1,142 +1,298 @@
-//
-// Created by Ruben on 27/06/2023.
-//
+
+
 #include <unistd.h>
 #include <cstring>
-#include <iostream>
+#include <cstdlib>
 #define MAX_SIZE 100000000
-#define MAX_ORDER 10
-#define FREEBLOCKS 32
 
+// size under 64 bytes
 struct MallocMetadata {
     size_t size;
     bool is_free;
-
     MallocMetadata* next;
     MallocMetadata* prev;
 };
-bool flag_init=false;
 
-int num_free_blocks=0;
-int num_free_bytes=0;
-int num_allocated_blocks=0;
-int num_allocated_bytes=0;
-int num_meta_data_bytes=0;
-int size_meta_data=sizeof (MallocMetadata);
+int num_free_blocks = 0;
+int num_free_bytes = 0;
+int num_allocated_blocks = 0;
+int num_allocated_bytes = 0;
+int num_meta_data_bytes = 0;
+int size_meta_data = sizeof(MallocMetadata);
 
-MallocMetadata* first = 0;
+const int MAX_ORDER = 10;
+const size_t MIN_BLOCK_SIZE = 128;
+const size_t MAX_BLOCK_SIZE = MIN_BLOCK_SIZE << MAX_ORDER;
+bool initFlag= false;
+
+MallocMetadata* free_lists[MAX_ORDER + 1] = {nullptr};
+MallocMetadata* mmap_blocks = nullptr;
 
 
-void initList() {
-    flag_init=true;
+// Helper function to get the order of a block based on its size
+int get_order(size_t size) {
+    int order = 0;
+    while (size > MIN_BLOCK_SIZE) {
+        size >>= 1;
+        order++;
+    }
+    return order;
+}
 
-    MallocMetadata* prev = nullptr;
-    MallocMetadata* curr = first;
-    for (int i=0; i<FREEBLOCKS; i++) {
-        curr = (MallocMetadata *) sbrk(size_meta_data);
-        curr->is_free = true;
-        curr->size = 128 * 1024;
-        curr->next = nullptr;
-        curr->prev = prev;
-        prev->next = curr;
-        prev = curr;
+void insertBlockInFreeList(MallocMetadata* ptr) {
+    int order = get_order(ptr->size);
+    if (free_lists[order] == nullptr) {
+        free_lists[order] = ptr;
+        ptr->next = nullptr;
+        ptr->prev = nullptr;
+    } else {
+        free_lists[order]->prev = ptr;
+        ptr->next = free_lists[order];
+        ptr->prev = nullptr;
+        free_lists[order] = ptr;
+    }
+}
+
+// Helper function to split a block into two buddies
+void split_block(MallocMetadata* block, int block_order) {
+    int buddy_order = block_order - 1;
+    size_t buddy_size = MIN_BLOCK_SIZE << buddy_order;
+
+    MallocMetadata* buddy = (MallocMetadata*)((char*)block + buddy_size);
+    buddy->size = buddy_size;
+    buddy->is_free = true;
+    buddy->next = free_lists[buddy_order];
+    buddy->prev = nullptr;
+    if (free_lists[buddy_order] != nullptr) {
+        free_lists[buddy_order]->prev = buddy;
+    }
+    free_lists[buddy_order] = buddy;
+}
+
+// Helper function to merge two buddy blocks
+void merge_blocks(MallocMetadata* block, int block_order) {
+    int buddy_order = block_order - 1;
+    size_t buddy_size = MIN_BLOCK_SIZE << buddy_order;
+
+    MallocMetadata* buddy = (MallocMetadata*)((char*)block - buddy_size);
+    if (buddy->is_free && buddy->size == buddy_size) {
+        // Remove buddy from free list
+        if (buddy->prev != nullptr) {
+            buddy->prev->next = buddy->next;
+        } else {
+            free_lists[buddy_order] = buddy->next;
+        }
+        if (buddy->next != nullptr) {
+            buddy->next->prev = buddy->prev;
+        }
+
+        // Merge blocks and update metadata
+        block = buddy < block ? buddy : block;
+        block->size <<= 1;
+        merge_blocks(block, block_order + 1);
+    } else {
+        // Add the current block to the free list
+        block->is_free = true;
+        block->next = free_lists[block_order];
+        block->prev = nullptr;
+        if (free_lists[block_order] != nullptr) {
+            free_lists[block_order]->prev = block;
+        }
+        free_lists[block_order] = block;
     }
 }
 
 
+//create 32 blocks and put them in the list of order
+void initOrderList() {
+    initFlag = true;
+    MallocMetadata *prev = nullptr;
+    for (int i = 0; i < 32; ++i) {
+        MallocMetadata *newptr = (MallocMetadata *) sbrk(MAX_BLOCK_SIZE);
+        if (newptr == (void *) -1) {
+            return;
+        }
+        newptr->size = MAX_BLOCK_SIZE;
+        newptr->is_free = true;
+        newptr->next = nullptr;
+        newptr->prev = prev;
+        prev = newptr;
+        if (prev != nullptr) {
+            prev->next = newptr;
+        }
+        insertBlockInFreeList(newptr);
+    }
+}
 
-
-
-
-//Searches for a free block with at least ‘size’ bytes or allocates (sbrk()) one if none are
+// Searches for a free block with at least 'size' bytes
 void* smalloc(size_t size) {
-    if (size == 0||size>MAX_SIZE) {
+    if (size == 0 || size > MAX_SIZE) {
         return nullptr;
     }
-    if (!flag_init){
-        initList();
-        }
 
-    //search for a free block
-    for (MallocMetadata *ptr = first; ptr != nullptr; ptr = ptr->next) {
-        if (ptr->is_free && ptr->size >= size) {
-            ptr->is_free = false;
+    if (!initFlag){
+       initOrderList();
+    }
+
+    int order = get_order(size + size_meta_data);
+    for (int i = order; i <= MAX_ORDER; i++) {
+        MallocMetadata* block = free_lists[i];
+        if (block != nullptr) {
+            // Remove block from free list
+            free_lists[i] = block->next;
+            if (block->next != nullptr) {
+                block->next->prev = nullptr;
+            }
+
+            // Check if the block can be split further
+            while (i > order) {
+                split_block(block, i);
+                i--;
+            }
+
+            block->is_free = false;
             num_allocated_blocks++;
-            num_allocated_bytes += size;
+            num_allocated_bytes += block->size - size_meta_data;
             num_free_blocks--;
-            num_free_bytes -= size;
-            return (void*) (ptr + sizeof (MallocMetadata));
+            num_free_bytes -= block->size;
+            return (void*)(block + 1);
         }
     }
+
+    // If no suitable block is found, send exception
+
 }
 
-//Allocates a new memory block of size ‘size’ bytes.
-void* scalloc(size_t num, size_t size){
-    void* new_ptr= smalloc(num*size);
-    std::memset(new_ptr,0,num*size);
+// Allocates a new memory block of size 'size' bytes.
+void* scalloc(size_t num, size_t size) {
+    void* new_ptr = smalloc(num * size);
+    if (new_ptr != nullptr) {
+        std::memset(new_ptr, 0, num * size);
+    }
     return new_ptr;
 }
 
-//Frees the memory space pointed to by ‘ptr’. If ‘ptr’ is NULL, no operation is performed.
-void sfree(void* p){
-    if (p== nullptr){
+// Frees the memory space pointed to by 'ptr'. If 'ptr' is NULL, no operation is performed.
+void sfree(void* p) {
+    if (p == nullptr) {
         return;
     }
-    MallocMetadata* ptr = (MallocMetadata*) p;
-    ptr -= sizeof (MallocMetadata);
-    if (ptr->is_free){
+    MallocMetadata* ptr = (MallocMetadata*)p;
+    ptr--;
+    if (ptr->is_free) {
         return;
     }
-    ptr->is_free=true;
+    ptr->is_free = true;
     num_free_blocks++;
-    num_free_bytes+=ptr->size;
-    return;
+    num_free_bytes += ptr->size;
+    merge_blocks(ptr, get_order(ptr->size));
 }
 
-//Changes the size of the memory block pointed to by ‘ptr’ to ‘size’ bytes and returns a pointer to the
-void* srealloc(void* oldp, size_t size){
-    if (oldp== nullptr){
+// Changes the size of the memory block pointed to by 'ptr' to 'size' bytes and returns a pointer to the
+void* srealloc(void* oldp, size_t size) {
+    if (oldp == nullptr) {
         return smalloc(size);
     }
-    if (size==0||size>MAX_SIZE){
+    if (size == 0 || size > MAX_SIZE) {
         sfree(oldp);
         return nullptr;
     }
-    MallocMetadata* ptr = (MallocMetadata*) oldp;
-    ptr -= sizeof (MallocMetadata);
-    if (ptr->size>=size){
+    MallocMetadata* ptr = (MallocMetadata*)oldp;
+    ptr--;
+    size_t old_size = ptr->size - size_meta_data;
+    if (ptr->size >= size + size_meta_data) {
+        // Check if the block can be split further
+        int order = get_order(size + size_meta_data);
+        while (ptr->size > MIN_BLOCK_SIZE << order) {
+            split_block(ptr, order);
+            order++;
+        }
+
         return oldp;
+    } else {
+        void* new_ptr = smalloc(size);
+        if (new_ptr != nullptr) {
+            std::memmove(new_ptr, oldp, old_size);
+        }
+        sfree(oldp);
+        return new_ptr;
     }
-    void* new_ptr = smalloc(size);
-    if (new_ptr== nullptr){
-        return nullptr;
-    }
-    std::memmove(new_ptr,oldp,ptr->size);
-    sfree(oldp);
-    return new_ptr;
 }
 
 
-size_t _num_free_blocks(){
+// Allocates a new memory block of size 'size' bytes using mmap().
+void* smmap(size_t size) {
+    if (size == 0 || size > MAX_SIZE) {
+        return nullptr;
+    }
+
+    MallocMetadata* block = (MallocMetadata*)mmap(nullptr, size + size_meta_data, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (block == (void*)-1) {
+        return nullptr;
+    }
+    block->size = size + size_meta_data;
+    block->is_free = false;
+    block->next = mmap_blocks;
+    block->prev = nullptr;
+    if (mmap_blocks != nullptr) {
+        mmap_blocks->prev = block;
+    }
+    mmap_blocks = block;
+
+    num_allocated_blocks++;
+    num_allocated_bytes += size;
+    num_meta_data_bytes += size_meta_data;
+    return (void*)(block + 1);
+}
+
+// Frees the memory space pointed to by 'ptr' that was allocated using mmap().
+void smunmap(void* p) {
+    if (p == nullptr) {
+        return;
+    }
+    MallocMetadata* ptr = (MallocMetadata*)p;
+    ptr--;
+    if (ptr->is_free) {
+        return;
+    }
+
+    // Remove block from mmap blocks list
+    if (ptr->prev != nullptr) {
+        ptr->prev->next = ptr->next;
+    } else {
+        mmap_blocks = ptr->next;
+    }
+    if (ptr->next != nullptr) {
+        ptr->next->prev = ptr->prev;
+    }
+
+    num_allocated_blocks--;
+    num_allocated_bytes -= ptr->size - size_meta_data;
+    num_meta_data_bytes -= size_meta_data;
+
+    munmap(ptr, ptr->size);
+}
+
+size_t _num_free_blocks() {
     return num_free_blocks;
 }
 
-size_t _num_free_bytes(){
+size_t _num_free_bytes() {
     return num_free_bytes;
 }
 
-size_t _num_allocated_blocks(){
+size_t _num_allocated_blocks() {
     return num_allocated_blocks;
 }
 
-size_t _num_allocated_bytes(){
+size_t _num_allocated_bytes() {
     return num_allocated_bytes;
 }
 
-size_t _num_meta_data_bytes(){
+size_t _num_meta_data_bytes() {
     return num_meta_data_bytes;
 }
 
-size_t _size_meta_data(){
+size_t _size_meta_data() {
     return size_meta_data;
 }
